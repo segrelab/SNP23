5575e4eeec5bfb5bb03174fc30c0672dd8e00a6f
Helen Scott <helengracescott@gmail.com>
2024-09-30 16:56:15 -0400

    Add Joseline's original script

---
+++ reads_to_vcf.sh
@@ -0,0 +1,44 @@
+#!/bin/bash -l
+#$ -l h_rt=75:00:00
+#$ -j y
+#$ -P hfsp
+#$ -pe omp 10
+
+module load bwa/0.7.17 
+module load samtools
+module load trimmomatic/0.36
+module load bcftools
+input_fi=/projectnb2/hfsp/Challenge21/joseline/data_info/paths_label_only.csv
+{
+  read 
+  while IFS=, read -r ref label;do
+  #setting up variables for inputs, file names and etc
+    directory=/projectnb2/hfsp/Challenge21/joseline/data_for_analysis/$label-4500T/
+    read_1="$directory"200219Seg_"$label"_1_sequence.fastq.gz
+    read_2="$directory"200219Seg_"$label"_2_sequence.fastq.gz
+
+    trim_1="$directory"trimmed_"$label"_1_sequence.fastq.gz
+    trimu_1="$directory"trimmedu_"$label"_1_sequence.fastq.gz
+
+    trim_2="$directory"trimmed_"$label"_2_sequence.fastq.gz
+    trimu_2="$directory"trimmedu_"$label"_2_sequence.fastq.gz
+
+    bam_file="$directory""$label".bam
+    sorted_bam_file="$directory""$label".sorted.bam
+    vcf="$directory""$label".vcf
+    f_vcf="$directory"filtered_"$label".vcf
+
+  #actually runnning tools 
+    trimmomatic PE -threads 10 $read_1 $read_2 \ $trim_1 $trimu_1 \ $trim_2 $trimu_2 \ ILLUMINACLIP:/usr4/bf527/smit2/.conda/pkgs/trimmomatic-0.39-1/share/trimmomatic/adapters/NexteraPE-PE.fa:2:30:10:1:TRUE
+
+    bwa index $ref
+    samtools faidx $ref
+    bwa mem $ref $trim_1 $trim_2 | samtools view -S -b > $bam_file
+    samtools sort $bam_file -o $sorted_bam_file
+    
+    samtools faidx $ref
+    bcftools mpileup -B -q 30 -g 50 -Ou -f $ref $sorted_bam_file | bcftools call --ploidy 1 -m -A > $vcf
+
+    bcftools filter -s LowQual $vcf | bcftools view -v snps > $f_vcf
+  done 
+}< $input_fi
