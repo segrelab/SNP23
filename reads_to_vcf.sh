9e8f79953ad5c64ca82744d31699372425c114b0
Helen Scott <helengracescott@gmail.com>
2024-10-01 12:33:35 -0400

    Use my copy of the raw files

--- reads_to_vcf.sh
+++ reads_to_vcf.sh
@@ -46,7 +46,7 @@ while IFS=, read -r sample_number sample_name species_name strain_id in_IAMM acc
     label="${sample_name%-4500T}"
 
     # Setting up variables for inputs, file names and etc
-    directory=/projectnb2/hfsp/Challenge21/joseline/data_for_analysis/$sample_name
+    directory=results/raw_files/$sample_name
     read_1="$directory"/200219Seg_"$label"_1_sequence.fastq.gz
     read_2="$directory"/200219Seg_"$label"_2_sequence.fastq.gz
 
