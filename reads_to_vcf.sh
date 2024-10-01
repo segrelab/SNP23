00166fd1733dd70cfd7fe20ddc75b0b4d050639f
Helen Scott <helengracescott@gmail.com>
2024-10-01 11:50:47 -0400

    Remove unnecessary spaces

--- reads_to_vcf.sh
+++ reads_to_vcf.sh
@@ -101,10 +101,10 @@ while IFS=, read -r sample_number sample_name species_name strain_id in_IAMM acc
 
     # Append the sample name and counts to the CSV file
     echo "$sample_name,$total_reads,$mapped_reads,$unmapped_reads" >> $output_csv
-    
+
     # Sort the bam file by genomic coordinates and save the sorted output to a new file
     samtools sort $bam_file -o $sorted_bam_file
-    
+
     bcftools mpileup -B -q 30 -g 50 -Ou -f $ref $sorted_bam_file | bcftools call --ploidy 1 -m -A > $vcf
 
     bcftools filter -s LowQual $vcf | bcftools view -v snps > $f_vcf
