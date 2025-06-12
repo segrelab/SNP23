ad1076d352b0b606b640f44b6f55e1d15844618d
Helen Scott <helengracescott@gmail.com>
2025-06-12 08:53:22 -0400

    Add gdtools to pipeline

--- run_all_breseq.sh
+++ run_all_breseq.sh
@@ -78,8 +78,13 @@ while IFS=, read -r strain_id species_name plasmidsaurus_id gbk_ref_genome path_
         continue
     fi
 
-    # Add breseq command to the commands file
-    echo "breseq -r $ref -o ${out_dir}/plasmidsaurus_vs_ref $in_dir/${plasmidsaurus_id}_R1_001.fastq.gz $in_dir/${plasmidsaurus_id}_R2_001.fastq.gz" >> "$commands_file"
+    # Add breseq and gdtools commands to the commands file
+    # The commands are chained together since gdtools depends on the output of breseq
+    # gdtools annotates the breseq with information about mutations
+    # (what genes they affect, amino acid substitutions, etc.)
+    # This information is currently only in the html file, which is hard to
+    # parse programmatically, so this converts it to TSV
+    echo "breseq -r $ref -o ${out_dir}/plasmidsaurus_vs_ref $in_dir/${plasmidsaurus_id}_R1_001.fastq.gz $in_dir/${plasmidsaurus_id}_R2_001.fastq.gz && gdtools ANNOTATE -o ${out_dir}/plasmidsaurus_vs_ref/output/output.tsv -r $ref -f TSV ${out_dir}/plasmidsaurus_vs_ref/output/output.gd" >> "$commands_file"
 
     # Process positive control if present
     if [ -n "$pos_cntrl_genome" ] && [ -n "$pos_cntrl_genome_path" ]; then
@@ -87,7 +92,7 @@ while IFS=, read -r strain_id species_name plasmidsaurus_id gbk_ref_genome path_
         pos_cntrl_genome_trimmed=$(echo "$pos_cntrl_genome" | xargs | tr -d '\r')
         pos_cntrl_ref="${pos_cntrl_path_trimmed}/${pos_cntrl_genome_trimmed}"
 
-        echo "breseq -r $pos_cntrl_ref -o ${out_dir}/pos_cntrl $in_dir/${plasmidsaurus_id}_R1_001.fastq.gz $in_dir/${plasmidsaurus_id}_R2_001.fastq.gz" >> "$commands_file"
+        echo "breseq -r $pos_cntrl_ref -o ${out_dir}/pos_cntrl $in_dir/${plasmidsaurus_id}_R1_001.fastq.gz $in_dir/${plasmidsaurus_id}_R2_001.fastq.gz && gdtools ANNOTATE -o ${out_dir}/pos_cntrl/output/output.tsv -r $pos_cntrl_ref -f TSV ${out_dir}/pos_cntrl/output/output.gd" >> "$commands_file"
     fi
 
     # Process negative control if present
@@ -96,7 +101,7 @@ while IFS=, read -r strain_id species_name plasmidsaurus_id gbk_ref_genome path_
         neg_cntrl_genome_trimmed=$(echo "$neg_cntrl_genome" | xargs | tr -d '\r')
         neg_cntrl_ref="${neg_cntrl_path_trimmed}/${neg_cntrl_genome_trimmed}"
 
-        echo "breseq -r $neg_cntrl_ref -o ${out_dir}/neg_cntrl $in_dir/${plasmidsaurus_id}_R1_001.fastq.gz $in_dir/${plasmidsaurus_id}_R2_001.fastq.gz" >> "$commands_file"
+        echo "breseq -r $neg_cntrl_ref -o ${out_dir}/neg_cntrl $in_dir/${plasmidsaurus_id}_R1_001.fastq.gz $in_dir/${plasmidsaurus_id}_R2_001.fastq.gz && gdtools ANNOTATE -o ${out_dir}/neg_cntrl/output/output.tsv -r $neg_cntrl_ref -f TSV ${out_dir}/neg_cntrl/output/output.gd" >> "$commands_file"
     fi
 
 done < "$input_fi"
